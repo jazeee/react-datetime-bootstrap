c7ff84e8020093968b890dd6f528aebce8eb0a30
Jaz Singh <jsingh@grailbio.com>
2017-10-17 10:19:15 -0700

    deps: update to react 16
    Summary:
    Update to react 16, update all related packages. Update to latest
    Enzyme.
    Fix tests

--- scripts/mocha_runner.js
+++ scripts/mocha_runner.js
@@ -7,14 +7,14 @@ const { document } = (new JSDOM('')).window;
 global.document = document;
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-        exposedProperties.push(property);
-        global[property] = document.defaultView[property];
-    }
+	if (typeof global[property] === 'undefined') {
+		exposedProperties.push(property);
+		global[property] = document.defaultView[property];
+	}
 });
 
 global.navigator = {
-    userAgent: 'node.js'
+	userAgent: 'node.js'
 };
 
 documentRef = document;
